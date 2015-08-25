a9987907873015f6a13b4e9f8161247628f86614
Heather MacPhail <hmacphail@gmail.com>
2015-08-25 11:07:07 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,8 +18,8 @@ var stickyHeaders = (function() {
               .height(thisSticky.outerHeight());             
       });
 
-      window_obj.off("scroll.stickies").on("scroll.stickies", function() {
-          _whenScrolling();     
+      window_obj.on("scroll", function(e) {
+        _whenScrolling();
       });
     }
   };
@@ -37,10 +37,12 @@ var stickyHeaders = (function() {
             nextStickyPosition = nextSticky.data('originalPosition') - thisSticky.data('originalHeight');
 
         thisSticky.addClass("fixed");
+        
+        //thisSticky.css("position", "fixed");
+        //thisSticky.css("transform", "translate(0,0)");
 
         if (nextSticky.length > 0 && thisSticky.offset().top >= nextStickyPosition) {
-
-          thisSticky.addClass("absolute").css("top", nextStickyPosition);
+          thisSticky.addClass("absolute").css("transform", "translate(0," + (nextStickyPosition - stickyPosition) + "px)");
         }
 
       } else {
@@ -48,11 +50,15 @@ var stickyHeaders = (function() {
         var prevSticky = stickies_obj.eq(i - 1);
 
         thisSticky.removeClass("fixed");
+        //thisSticky.css("position", "relative");
+        //thisSticky.css("transform", "none");
 
         if (prevSticky.length > 0 && window_obj.scrollTop() <= thisSticky.data('originalPosition') - thisSticky.data('originalHeight')) {
-
+            //&& window_obj.scrollTop() > prevSticky.data('originalPosition')) {
           prevSticky.removeClass("absolute").removeAttr("style");
+          //prevSticky.css("position", "fixed").css("transform", "translate(0,0)");
         }
+
       }
     });
   };
